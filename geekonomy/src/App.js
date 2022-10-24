b6dae43b79ba1b7217278f0b2a255aada4c9090e
chandrasekhar9991 <nagellancsreddy91@gmail.com>
2022-10-24 20:39:47 +0530

    firstcommit

---
+++ geekonomy/src/App.js
@@ -0,0 +1,19 @@
+//import { Home } from "./components/Home";
+
+import BasicTabs from "./components/Home";
+
+//import LabTabs from "./components/Home";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <BasicTabs/>
+     
+    </div>
+  );
+}
+
+export default App;
